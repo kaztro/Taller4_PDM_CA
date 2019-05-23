d6d8a4050fc3718b13951635018dd23279d1d6d6
kaztro <00229017@uca.edu.sv>
2019-05-23 15:36:39 -0600

    Inicializando rama

--- app/src/main/java/com/example/taller4_pdm_ca/activities/MainActivity.kt
+++ app/src/main/java/com/example/taller4_pdm_ca/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.taller4_pdm_ca
+package com.example.taller4_pdm_ca.activities
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.example.taller4_pdm_ca.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
