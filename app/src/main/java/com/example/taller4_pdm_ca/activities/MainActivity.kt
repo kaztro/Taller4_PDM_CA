62486e52c93248890fefaf723b6477a1b328eee1
kaztro <00229017@uca.edu.sv>
2019-05-24 19:27:05 -0600

    Bueno ya perdi mi nota :(

--- app/src/main/java/com/example/taller4_pdm_ca/activities/MainActivity.kt
+++ app/src/main/java/com/example/taller4_pdm_ca/activities/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.taller4_pdm_ca.activities
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taller4_pdm_ca.R
-
+import com.example.taller4_pdm_ca.adapters.BookAdapter
+import com.example.taller4_pdm_ca.pojos.Book
+import com.example.taller4_pdm_ca.viewmodel.BookViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,32 +17,46 @@ class MainActivity : AppCompatActivity() {
     lateinit var bookViewModel: BookViewModel
     private var flag = true
     private var switch = true
-    private lateinit var adapter: Adapter
+    private lateinit var bookAdapter: BookAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        bookAdapter = BookAdapter(this, { book: Book -> favoriteBook(book) }, { book: Book -> triggerActivity(book) })
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
+        //var bookAdapter = BooksAdapter(this) { book:Book->bookOnClicked(book)}
+        rv_books.adapter = bookAdapter
+        rv_books.layoutManager = LinearLayoutManager(this)
+
+        bookViewModel = ViewModelProviders.of(this).get(BookViewModel::class.java)
+
+
+
+        bookViewModel.allBooks.observe(this, Observer { books ->
+            bookAdapter.setBooks(books)
+        })
+        initClicksListeners()
+    }
+
+    private fun initClicksListeners(){
+        btn_favorites.setOnClickListener(){
+            bookViewModel.favoriteBooks.observe(this, Observer { favBooks -> //This favorites books deberia estar en ViewModel
+                bookAdapter.setBooks(favoriteBooks)
+            })
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun triggerActivity(book: Book) {
+        val bundleBook = Bundle()
+            bundleBook.putParcelable("BOOK", book)
+        startActivity(Intent(this, ViewerActivity::class.java).putExtras(bundleBook))
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun bookFavorito(book: Book) {
+        bookViewModel.favoriteBooks.observe(this, Observer { favorites ->
+            bookAdapter.setBooks(favorites)
+        })
+        bookViewModel.chekinoutF(book)
     }
 }
